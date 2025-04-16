42644603860002cb973904cb3dac71767f10c707
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-04-16 12:56:43 +0530

    update

---
+++ tests/UserTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\User;
+
+class UserTest extends TestCase {
+
+  public function testClassConstructor()
+  {
+    $user = new User(18, 'John');
+
+    $this->assertSame('John', $user->name);
+    $this->assertSame(18, $user->age);
+    $this->assertEmpty($user->favorite_movies);
+  }
+}
